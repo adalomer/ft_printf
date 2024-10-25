423b73d12abaebc322f3d34e811c5e7e94d67d96
Omer Adali <adalomer60@gmail.com>
2024-10-25 16:12:58 +0300

    %70

--- ft_printf/ft_hexadecimal.c
+++ ft_printf/ft_hexadecimal.c
@@ -1,75 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_hexdecimal.c                                    :+:      :+:    :+:   */
+/*   ft_hexadecimal.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omadali <adalomer60@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 23:52:46 by omadali           #+#    #+#             */
-/*   Updated: 2024/10/24 20:16:11 by omadali          ###   ########.fr       */
+/*   Updated: 2024/10/25 13:32:03 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int  hexadecimal(va_list args, int *d,int a)
-{
+#include <stdarg.h>
 
-char *b;
-char *c;
-int f;
-int g;
-
-f = args;
-b = "0123456789abcdef";
-c = "0123456789ABCDEF";
-while(f > 0)
-{
-	f / 16;
-	g++;
-}
-if ( a = 1)
-{
-	if (g > 0)
-	{
-	*d += putchar( b[f % 16 * g]);
-	g--;
-	}
-	return(*d);
-}
-else
-{
-	if (g > 0)
-	{
-	*d += putchar( c[f % 16 * g]);
-	g--;
-	}
-	return(*d);
-}
-}
-/*#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 int ft_putchar(int a)
 {
     write(1,&a,1);
     return (1);
 }
 
-int  ft_hex(int h, int *d,int a)
+void  ft_hexadecimal(int h, int *d,int a)
 {
     char    *e;
 
     e = "0123456789abcdef";
     if (a == 1)
         e = "0123456789ABCDEF";
-    if (n > 15)
-        ft_hex(h / 16,d,);
-    ft_putchar_fd(hex[n % 16]);
-    if (n < 16)
+    if (h > 15)
+        ft_hexadecimal(h / 16,d,a);
+    ft_putchar(e[h % 16]);
+    if (h < 16)
         *d = *d + 1;
 }
-int main()
-{
-  
-  int j = 5;
-  ft_hexadecimal(55,&j,1);
-}*/,
